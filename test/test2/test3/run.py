83c19ebb9456e66f0b417f7e5d034f664c10a1cc
Robert Chen <robert.chen.kfc@gmail.com>
2025-06-07 11:18:09 -0700

    Added more failing tests

---
+++ test/test2/test3/run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+print("Hello world from python")
+exit(-1)
---
+++ test/test4/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+int main() {
+  printf("Hello from test4!\n");
+  printf("Just another print!\n");
+}
